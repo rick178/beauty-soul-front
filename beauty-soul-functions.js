f3b5adfe7d896d9bdb38cb897d194892d77c3769
rick <rick178@hotmail.com>
2025-09-23 20:35:58 -0600

    Fixing Hamburguer Menu

--- beauty-soul-functions.js
+++ beauty-soul-functions.js
@@ -1,7 +1,14 @@
 
 //elementos del DOM
 
-let hamburgerButton =  document.getElementById("menu-toggle");
-hamburgerButton.addEventListener("click", (event)=>{
-    alert("Menu Hamburguesa");
-}) 
+// let hamburgerButton =  document.getElementById("menu-toggle");
+// hamburgerButton.addEventListener("click", (event)=>{
+//     alert("Menu Hamburguesa");
+// }) 
+
+const toggle = document.getElementById("menu-toggle");
+const menu = document.querySelector(".menu-beauty");
+
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
