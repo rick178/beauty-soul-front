582d7fdca2ac23b54d9ca27582ac12f3321e5df7
rick <rick178@hotmail.com>
2025-09-27 23:49:59 -0600

    Adding photo slider

--- beauty-soul-functions.js
+++ beauty-soul-functions.js
@@ -12,3 +12,26 @@
 // toggle.addEventListener("click", () => {
 //   menu.classList.toggle("active");
 // });
+
+const logoBS = document.getElementById("logo-bs");
+const sliderBS = document.querySelector(".beautysoul-slider");
+
+logoBS.addEventListener("animationend", () => {
+    console.log("Animacion terminada");
+    sliderBS.style.display = "block";
+    sliderBS.style.opacity = 1; 
+});
+
+logoBS.addEventListener("webkitAnimationEnd", () => {
+    console.log("Animacion terminada");
+        sliderBS.style.display = "block";
+        sliderBS.style.opacity = 1; 
+});
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const sliderBS = document.querySelector(".beautysoul-slider");
+
+//     setTimeout(() => {
+//         sliderBS.style.display = "block";
+//     }, 3000); // 3 segundos = duración de tu animación
+// });
